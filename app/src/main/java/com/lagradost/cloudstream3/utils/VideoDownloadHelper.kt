56436c7a3c0f191fc838860a6a423ae654155e0c
navianwadda <navianwadda@outlook.com>
2026-04-02 16:09:31 +0600

    Update VideoDownloadHelper.kt

--- app/src/main/java/com/lagradost/cloudstream3/utils/VideoDownloadHelper.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/VideoDownloadHelper.kt
@@ -1,13 +1,1 @@
-package com.lagradost.cloudstream3.utils
 
-import com.lagradost.cloudstream3.utils.downloader.DownloadObjects
-
-/**
- * Compatibility shim — VideoDownloadHelper classes were moved to DownloadObjects.
- */
-object VideoDownloadHelper {
-    typealias DownloadCached        = DownloadObjects.DownloadCached
-    typealias DownloadEpisodeCached = DownloadObjects.DownloadEpisodeCached
-    typealias DownloadHeaderCached  = DownloadObjects.DownloadHeaderCached
-    typealias ResumeWatching        = DownloadObjects.ResumeWatching
-}
